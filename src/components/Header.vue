3fe8539a03d9f03234a4936d73791215f0562125
Ilyoskhuja <mr.ikromhodjayev@gmail.com>
2021-11-12 16:50:10 +0500

    ft

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<nav class="navbar navbar-light bg-light mb-5">
+  <div class="container-fluid justify-content-center">
+    <span class="navbar-brand mb-0 h1">Match recordings </span>
+  </div>
+</nav>    
+</template>
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+nav{
+  height: 80px;
+}
+</style>
